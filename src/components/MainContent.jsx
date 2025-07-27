fb991d4a500afef1e09774752ac6cca74622e4a3
Alisena Danishwer <danishweralisena@gmail.com>
2025-07-27 10:54:04 +0800

    Apply inlinline css styling to all components

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f9f9f9', fontSize: '16px' }}>
+      <p>Welcome to my list of favorite cities around the world!</p>
+    </main>
+  );
+}
+
+export default MainContent;
